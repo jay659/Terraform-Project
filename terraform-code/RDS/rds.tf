f2aa71ee8d2ab462b15ff8b9fde06af8de75ae61
Arlin George <a.george748@mybvc.ca>
2023-11-26 12:03:09 -0700

    Updated EC2 and RDS code

--- terraform-code/RDS/rds.tf
+++ terraform-code/RDS/rds.tf
@@ -14,6 +14,6 @@ resource "aws_db_instance" "main" {
   username             = "admin"
   password             = "group3@3102"
   publicly_accessible = false
-  vpc_security_group_ids = [aws_security_group.main.id]
-  vpc_id               = aws_vpc.main.id
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  vpc_id               = aws_vpc.my_vpc.id
 }
